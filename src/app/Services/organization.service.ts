38ba119d0d65b83673437841832100bfe1c95dc8
Vikas Palve <vikas.palve@mindbowser.com>
2019-04-09 20:23:03 +0530

    Initial commit

---
+++ src/app/Services/organization.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
+import { environment } from './../../environments/environment'
+import { from } from 'rxjs';
+@Injectable({
+  providedIn: 'root'
+})
+export class OrganizationService {
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  getOrganizationListAPI(access_token, page, payload){
+    // return this.http.get("http://www.mocky.io/v2/5c80fc643100006313771ca8");
+    return this.http.post(environment.BaseURL+"orgnization/get-organization-list/"+page+"?access_token="+access_token,payload);
+  }
+
+  getOrganizationListByFilterAPI(access_token, page, payload){
+    return this.http.post(environment.BaseURL+"orgnization/get-organization-list/"+page+"?access_token="+access_token,payload);
+  }
+
+  updateOrgDetailsAPI(access_token, payload){
+    return this.http.post(environment.BaseURL+"orgnization/update-company?access_token="+access_token , payload);
+  }
+
+  suspenDeleteOrg(access_token, payload){
+   return this.http.post(environment.BaseURL+"orgnization/update-company?access_token="+access_token, payload);
+  }
+}
